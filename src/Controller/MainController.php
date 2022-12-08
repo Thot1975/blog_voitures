c9f4c46c3c4c3d8248afc62c312551dc0c621b6f
Thot1975 <stephane.aragones@gmail.com>
2022-12-08 11:52:52 +0100

    page acceuil marque plus detail par marque

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Marque;
 use App\Repository\AnnonceRepository;
+use App\Repository\MarqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(AnnonceRepository $annonceRepository,  MarqueRepository $marqueRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'annonces' => $annonceRepository->findAll()
+            'annonces' => $annonceRepository->findAll(),
+            'marques' => $marqueRepository->findAll()
+        ]);
+    }
+    #[Route('/tab/{id}', name: 'tab')]
+    public function tab(Marque $marque, AnnonceRepository $annonceRepository, MarqueRepository $marqueRepository): Response
+    {
+        return $this->render('main/marque.html.twig', [
+            'marq' => $marque,
+            'annonces' => $annonceRepository->findAll(),
+            'marques' => $marqueRepository->findAll(),
         ]);
     }
 }
